80249297c44878a506597bbd8f169579bd48b64b
Vakul16 <valusavakhul@gmail.com>
2021-07-13 01:29:35 +0530

    Tabs component and filter component

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TabContainer from "../Components/TabContainer";
+import Filter from "../Components/Filter";
+const HomePage = () => {
+  return (
+    <div className="home">
+      <TabContainer />
+      <Filter />
+    </div>
+  );
+};
+
+export default HomePage;
