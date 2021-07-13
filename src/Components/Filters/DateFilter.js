fedba861b4b419ed15d284b163b336749a2ae9a7
Vakul16 <valusavakhul@gmail.com>
2021-07-13 17:45:12 +0530

    Worked on date & time

--- src/Components/Filters/DateFilter.js
+++ src/Components/Filters/DateFilter.js
@@ -1,7 +1,65 @@
+import "date-fns";
 import React from "react";
+import Grid from "@material-ui/core/Grid";
+import DateFnsUtils from "@date-io/date-fns";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
 
-const DateFilter = () => {
-  return <div>Date</div>;
-};
+export default function DateFilter() {
+  // The first commit of Material-UI
+  const [selectedDate, setSelectedDate] = React.useState(
+    new Date("2014-08-18T21:11:54")
+  );
 
-export default DateFilter;
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
+  return (
+    <div>
+    <h4>SHOW RATINGS FOR:</h4>
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Grid container justifyContent="space-around">
+        <KeyboardDatePicker
+          disableToolbar
+          variant="inline"
+          format="MM/dd/yyyy"
+          margin="normal"
+          id="date-picker-inline"
+          label="Date picker inline"
+          value={selectedDate}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            "aria-label": "change date",
+          }}
+        />
+        TO
+        <KeyboardDatePicker
+          margin="normal"
+          id="date-picker-dialog"
+          label="Date picker dialog"
+          format="MM/dd/yyyy"
+          value={selectedDate}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            "aria-label": "change date",
+          }}
+        />
+        {/* <KeyboardTimePicker
+          margin="normal"
+          id="time-picker"
+          label="Time picker"
+          value={selectedDate}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change time',
+          }}
+        /> */}
+      </Grid>
+    </MuiPickersUtilsProvider>
+    </div>
+  );
+}
